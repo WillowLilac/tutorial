98260f27b98696fa8be925ebff197693ec2b69c3
sukuna2713 <sukuna2713@gmail.com>
2021-06-21 05:42:05 +0000

    add modal item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React, {useState} from "react";
+import Modal from "react-modal";
 import "./App.css";
 import { BookToRead } from "./BookToRead";
 import BookRow from "./BookRow";
+import BookSearchDialog from "./BookSearchDialog";
+
+//モーダル表示時にオーバーレイで覆うDOM領域を指定
+Modal.setAppElement("#root");
+
+//モーダルダイアログおよびオーバーレイの外観のスタイル設定
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.8)"
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    padding: 0,
+    transform: "translate(-50%, -50%)"
+  }
+};
 
 const dummyBooks: BookToRead[] = [
   {
@@ -26,6 +47,7 @@ const dummyBooks: BookToRead[] = [
 
 const App = () => {
   const [books,setBooks] = useState(dummyBooks);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   //削除処理の関数
   const handleBookDelete = (id: number) => {
@@ -43,6 +65,16 @@ const App = () => {
     setBooks(newBooks);
   }
 
+  //本を追加ボタンのクリック時の処理の関数
+  const handleAddClick = () => {
+    setModalIsOpen(true);
+  };
+
+  //モーダルを閉じる処理の関数
+  const handleModalClose = () => {
+    setModalIsOpen(false);
+  };
+
   const BookRows = books.map((b) => {
     return (
       <BookRow
@@ -58,11 +90,16 @@ const App = () => {
     <div className="App">
       <section className="nav">
         <h1>読みたい本リスト</h1>
-        <div className="button-like">本を追加</div>
+        <div className="button-like" onClick={handleAddClick}>本を追加</div>
       </section>
       <section className="main">
         {BookRows}
       </section>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModalClose}
+        style={customStyles}
+      ></Modal>
     </div>
   );
 };
